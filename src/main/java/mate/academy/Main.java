f25c28eb40291852e7b6bda21fde03a8eb0a91b0
Oleh Ridchenko <olehridchenko@gmail.com>
2025-09-04 20:27:04 +0300

    added edits from ai menthor

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,8 +1,15 @@
 package mate.academy;
 
 import mate.academy.lib.Injector;
+import mate.academy.model.CinemaHall;
 import mate.academy.model.Movie;
+import mate.academy.model.MovieSession;
+import mate.academy.service.CinemaHallService;
 import mate.academy.service.MovieService;
+import mate.academy.service.MovieSessionService;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class Main {
     private static final Injector injector = Injector.getInstance("mate.academy");
@@ -15,5 +22,39 @@ public class Main {
         movieService.add(fastAndFurious);
         System.out.println(movieService.get(fastAndFurious.getId()));
         movieService.getAll().forEach(System.out::println);
+
+        CinemaHall cinemaHall = new CinemaHall();
+        cinemaHall.setCapacity(50);
+        cinemaHall.setDescription("Small cinema hall.");
+
+        CinemaHallService cinemaHallService = (CinemaHallService) injector.getInstance(
+                CinemaHallService.class);
+        System.out.println(cinemaHallService.add(cinemaHall));
+        System.out.println(cinemaHallService.get(cinemaHall.getId()));
+        cinemaHallService.getAll().forEach(System.out::println);
+
+        MovieSession movieSession1 = new MovieSession();
+        movieSession1.setMovie(fastAndFurious);
+        movieSession1.setCinemaHall(cinemaHall);
+        movieSession1.setShowTime(LocalDateTime.of(2024, 5, 9, 23, 59, 59));
+
+        MovieSession movieSession2 = new MovieSession();
+        movieSession2.setMovie(fastAndFurious);
+        movieSession2.setCinemaHall(cinemaHall);
+        movieSession2.setShowTime(LocalDateTime.of(2024, 5, 10, 20, 30));
+
+        MovieSession movieSession3 = new MovieSession();
+        movieSession3.setMovie(fastAndFurious);
+        movieSession3.setCinemaHall(cinemaHall);
+        movieSession3.setShowTime(LocalDateTime.of(2024, 5, 11, 0, 0, 0));
+
+        MovieSessionService movieSessionService = (MovieSessionService) injector
+                .getInstance(MovieSessionService.class);
+        System.out.println(movieSessionService.add(movieSession1));
+        System.out.println(movieSessionService.add(movieSession2));
+        System.out.println(movieSessionService.add(movieSession3));
+        System.out.println(movieSessionService.get(movieSession1.getId()));
+        movieSessionService.findAvailableSessions(movieSession1.getId(),
+                LocalDate.of(2024, 5, 10)).forEach(System.out::println);
     }
 }
--- src/main/java/mate/academy/model/MovieSession.java
+++ src/main/java/mate/academy/model/MovieSession.java
@@ -29,6 +29,14 @@ public class MovieSession {
     public MovieSession() {
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public CinemaHall getCinemaHall() {
         return cinemaHall;
     }
